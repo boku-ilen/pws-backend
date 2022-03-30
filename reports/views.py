9749602da732076f2882be09f14d5927a5e59ca4
Karl <karl.bittner@hexaquo.at>
2022-03-30 11:21:43 +0200

    Some cleanup in Reports

--- reports/views.py
+++ reports/views.py
@@ -11,22 +11,27 @@ def get_unresolved_reports(request: HttpRequest) -> JsonResponse:
 
 
 def create_report(request: HttpRequest, text, email=None) -> JsonResponse:
+    """Create a new UserReport with content text and an optional contact email."""
+
     report = UserReport()
 
     report.text = text
     report.user_email = email
     report.save()
 
-    # TODO: Can this fail?
+    # It seems like `save()` doesn't return anything and can't raise exceptions, so we can always return success
 
     return JsonResponse({"success": True})
 
 
 def resolve_report(request: HttpRequest, report_id) -> JsonResponse:
-    report = UserReport.objects.get(id=report_id)
-    report.is_resolved = True
-    report.save()
+    """Set the UserReport with the given ID to 'resolved' if it exists."""
 
-    # TODO: Return error if there is no report with that ID
+    try:
+        report = UserReport.objects.get(id=report_id)
+        report.is_resolved = True
+        report.save()
 
-    return JsonResponse({"success": True})
+        return JsonResponse({"success": True})
+    except UserReport.DoesNotExist:
+        return JsonResponse({"success": False})
