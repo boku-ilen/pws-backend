62eff4d67d777f5580107c60e315fc88b81cf3f3
karl <karl.bittner@hexaquo.at>
2022-10-26 16:16:10 +0200

    Dockerize

--- pws/settings.py
+++ pws/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +82,11 @@ WSGI_APPLICATION = 'pws.wsgi.application'
 # Database settings
 DATABASES = {
     'default': {
-        'NAME': 'pws',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'pws',
-        'PASSWORD': 'PWS',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
         'PORT': '5432'
     },
 }
